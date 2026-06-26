4a737e854dc5b045a925e6731e08eb315f0f4660
Joao Pedro Oliveira Barbosa <95698131+Iohanan-Cephas@users.noreply.github.com>
2026-06-26 14:01:28 -0300

    foundations/05_joinStrings exercise done

--- foundations/05_joinStrings/joinStrings.js
+++ foundations/05_joinStrings/joinStrings.js
@@ -10,7 +10,16 @@
 // Add your code right below, good luck!
 
 
+const firstName = 'Carlos'
+const lastName = 'Stevenson'
+const thisYear = 1965
+const birthYear = 1947
+const fullName = `${firstName} ${lastName}`
+const age = thisYear - birthYear
 
+greeting = `Hello! My name is ${firstName} ${lastName} and I am ${thisYear - birthYear} years old.`
+
+console.log(this.greeting)
 
 
 // Do not change this
